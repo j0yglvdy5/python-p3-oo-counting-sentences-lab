ddee09451ccf61c84412f8595e60a497a1ea918d
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-06-19 13:24:23 +0300

    final

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+    def __init__(self, initial_value=''):
+        self._value = str(initial_value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+          print("The value must be a string.")
+        self._value = new_value
+
+    def is_sentence(self):
+        if self._value.endswith('.'):
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self._value.endswith('?'):
+            return True
+        else:
+            return False
+    def is_exclamation(self):
+        if self._value.endswith('!'):
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        # Split the value by '.', '!', and '?' and count the segments
+        sentences = [sentence.strip() for sentence in re.split(r'[.!?]', self._value) if sentence.strip()]
+        return len(sentences)
